5cad92924cb38b809b0f058777b8f178406306e1
Azat Rozygeldiyew <rozygeldi@mail.ru>
2023-02-12 18:13:54 +0500

    Это наш первый коммитЭ

---
+++ src/script.js
@@ -0,0 +1,38 @@
+'use strict';
+const number = 5;
+const string = "Hello";
+const boolean = true;
+null;
+undefined;
+const obj = {}
+
+console.log(4 / 0);
+console.log('string' * 9);
+let something;
+
+console.log(something);
+const persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(persone["name"]);
+const arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
+// alert("Hello World!!");
+// let answer=confirm("Are you here?");
+// console.log(answer);
+// const answer = +prompt("Eshek senmi ?", "Howa");
+// console.log(typeof (answer));
+// console.log("arr"+"-object");
+// console.log(4+"-object");
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2"===2);
+let isChecked = true,
+    isClose =false;
+console.log(isChecked || !isClose);
